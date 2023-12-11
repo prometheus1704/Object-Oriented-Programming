260fcf3a0ebf15bed06e18ca8ca60d8a67edf610
prometheus1704 <144023080+prometheus1704@users.noreply.github.com>
2023-12-11 18:45:27 +0530

    Prac2_StudentDatabase.cpp

--- Prac2_StudentDatabase.cpp
+++ Prac2_StudentDatabase.cpp
@@ -1,245 +1,251 @@
-#include<iostream>
-#include<string.h>
+#include <iostream>
 using namespace std;
-class Student
+
+class Stud
 {
-int roll_number;
-string name,dob;
-string addr;
-string licno;
-string phn;
-static int count;
 public:
-void getdata();
-void display();
-int search()
-{
-return roll_number;
-}
-void update_name();
-void update_dob();
-void update_addr();
-void update_licno();
-void update_phn();
+    int roll;
+    string name, clas, dob, address, dlno;
+    static int cnt=0;
+    long int phon;
+
+    void set_data();
+    void display();
+
+    int retroll()
+    {
+        return roll;
+    }
+    // string retname()
+    // {
+    //     return name;
+    // }
+    void getclass()
+    {
+        cin >> clas;
+    }
+
+    void getname()
+    {
+        cin >> name;
+    }
+
+    void getadd()
+    {
+        cin >> address;
+    }
+
+    void getdob()
+    {
+        cin >> dob;
+    }
+
+    void getdlno();
+    void getphon();
 };
-int Student::count=0;
-void Student::getdata()
+
+// int Stud ::cnt = 0;
+
+inline void Stud::getdlno()
 {
-count++;
-roll_number=count;
-cout<<"Enter the name of student:";
-cin.ignore();
-getline(cin,name);
-cout<<"Enter the Date of birth of the student :";
-cin>>dob;
-cout<<"Enter the address of the student :";
-cin.ignore();
-getline(cin,addr);
-cout<<"Enter the driving license number of the student :";
-getline(cin,licno);
-cout<<"Enter the phone number the student:";
-cin.ignore();
-getline(cin,phn);
+    cin >> dlno;
 }
-void Student::display()
+
+inline void Stud::getphon()
 {
-cout<<"Name of the student is "<<name<<endl;
-cout<<"Roll number is "<<roll_number<<endl;
-cout<<"Date of birth is "<<dob<<endl;
-cout<<"The Address is "<<addr<<endl;
-cout<<"Driving license number of the student is "<<licno<<endl;
-cout<<"Phone number of student is "<<phn<<endl;
+    cin >> phon;
 }
-inline void Student::update_dob()
+
+void Stud ::set_data()
 {
-cout<<"Enter the updated Date of birth of student : ";
-cin>>dob;
+    cnt++;
+    roll = cnt;
+    cout << "Enter the name :" << endl;
+    cin >> name;
+    cout << "Enter the address :" << endl;
+    cin >> address;
+    cout << "Enter the date of birth :" << endl;
+    cin >> dob;
+    cout << "Enter the class :" << endl;
+    cin >> clas;
+    cout << "Enter the dl number :" << endl;
+    cin >> dlno;
+    cout << "Enter the phone number :" << endl;
+    cin >> phon;
 }
-inline void Student::update_name()
+
+void Stud ::display()
 {
-cout<<"Enter the updated Name of student : ";
-cin.ignore();
-getline(cin,name);
-}
-inline void Student::update_addr()
-{
-cout<<"Enter the updated address of the student : ";
-cin.ignore();
-getline(cin,addr);
-}
-inline void Student::update_licno()
-{
-cout<<"Enter the updated license name of student : ";
-cin>>licno;
-}
-inline void Student::update_phn()
-{
-    cout<<"Enter the updated Phone number of student : ";
-cin>>phn;
+    cout << "---------------------------------------" << endl;
+    cout << "The roll no. of student is     : " << roll << endl;
+    cout << "The name of student is         : " << name << endl;
+    cout << "The class of student is        : " << clas << endl;
+    cout << "The address of student is      : " << address << endl;
+    cout << "The date of birth of student is: " << dob << endl;
+    cout << "The dial no. of student is     : " << dlno << endl;
+    cout << "The phone no. of student is    : " << phon << endl;
+    cout << "---------------------------------------" << endl;
 }
 
 int main()
 {
-Student s[10];
-int i,j,n,op,key,del;
-int flag,flag1,r,y,flags;
-int modify,option;
-cout<<"Enter the number of student data to be input: ";
-cin>>n;
-for(i=0;i<n;i++)
-{
-cout<<"----------------------------"<<endl;
-s[i].getdata();
-cout<<"----------------------------"<<endl;
-}
-flag=1;
-while (flag==1)
-{
-cout<<"---------MENU----------"<<endl;
-cout<<"1:Display the data of student "<<endl;
-cout<<"2:Search a student"<<endl;
-cout<<"3:Update the data"<<endl;
-cout<<"4:Delete a data of a student "<<endl;
-cout<<"5:Exit"<<endl;
-cout<<"ENTER YOUR CHOICE :"<<endl;
-cin>>op;
+    int n, i, k;
+    cout << "Enter the number of records: ";
+    cin >> n;
 
+    Stud s[n];
+    for (i = 0; i < n; i++)
+    {
+        s[i].set_data();
+    }
 
-switch(op)
-   {
-    case 1:
-        for (i=0;i<n;i++)
+    cout << "\n--------------------Menu--------------------" << endl;
+
+    do
+    {
+        cout << endl;
+        cout << "\n1.display   2.search   3.update  4.delete  5.exit" << endl;
+
+        cout << "\nEnter your choice: " << endl;
+        cin >> k;
+
+        switch (k)
         {
-        cout<<"----------------------------"<<endl;
-            s[i].display();
-            cout<<"----------------------------"<<endl;
-        }
-        break;
+        case 1:
 
-    case 2:
-        cout << "Enter the Roll number of the student to be searched: " ;
-        cin >> key;
-        flag1=0;
-        for (i=0;i<n;i++)
-        {
-            r = s[i].search();
-            if (r == key)
+            for (i = 0; i < n; i++)
             {
-                cout << "Match found" << endl;
-                cout<<"----------------------------"<<endl;
                 s[i].display();
-                cout<<"----------------------------"<<endl;
-                flag1 = 1;
             }
+
+            break;
+
+        case 2:
+            int flag;
+            flag = 0;
+            int key, r;
+            cout << "Enter the roll you want to search:" << endl;
+            cin >> key;
+
+            for (i = 0; i < n; i++)
+            {
+                r = s[i].retroll();
+                if (r == key)
+                {
+                    cout << "record found" << endl;
+                    s[i].display();
+                    flag = 1;
+                }
+            }
+            if (flag == 0)
+            {
+                cout << "record not found";
+            }
+            break;
+
+        case 3:
+            int modify, option, flags;
+            cout << "Enter the Roll number of student to updates the details: " << endl;
+            cin >> modify;
+            flags = 1;
+            while (flags == 1)
+            {
+
+                for (i = 0; i < n; i++)
+                {
+                    r = s[i].retroll();
+                    if (r == modify)
+                    {
+                        cout << endl;
+                        cout << "\n---------MENU----------" << endl;
+                        cout << "1:Change the name " << endl;
+                        cout << "2:Change the Address of the student " << endl;
+                        cout << "3:Change the Date of Birth" << endl;
+                        cout << "4.Change the Class of the student" << endl;
+                        cout << "5:Change the Driving license of the student " << endl;
+                        cout << "6:Change the Phone number of the student " << endl;
+                        cout << "7: Exit" << endl;
+                        cout << "ENTER YOUR CHOICE :" << endl;
+                        cin >> option;
+                        switch (option)
+                        {
+                        case 1:
+                            s[i].getname();
+                            s[i].display();
+                            break;
+
+                        case 2:
+                            s[i].getadd();
+                            s[i].display();
+                            break;
+
+                        case 3:
+                            s[i].getdob();
+                            s[i].display();
+                            break;
+
+                        case 4:
+                            s[i].getclass();
+                            s[i].display();
+                            break;
+
+                        case 5:
+                            s[i].getdlno();
+                            s[i].display();
+                            break;
+                        case 6:
+                            s[i].getphon();
+                            s[i].display();
+                            break;
+
+                        case 7:
+                            flags = 0;
+                            break;
+
+                        default:
+                            cout << "Incorrect choice Please enter the appropiate choice ";
+                        }
+                    }
+                }
+            }
+            break;
+
+            if (flags == 0)
+                break;
+        case 4:
+            int del;
+            cout << "The Roll number of student whose data should be delete: ";
+            cin >> del;
+
+            for (i = 0; i < n; i++)
+            {
+                r = s[i].retroll();
+                if (r == del)
+                {
+                    for (int j = i; j < n - 1; j++)
+                    {
+                        s[j] = s[j + 1];
+                    }
+                    n--;
+                }
+            }
+            for (i = 0; i < n; i++)
+            {
+
+                s[i].display();
+            }
+
+            break;
+
+        case 5:
+            cout << "thank you!";
+            break;
+
+        default:
+            cout << "Wrong choice";
+            break;
         }
-        if (flag1 == 0)
-        {
-            cout << "Match not found" << endl;
-        }
-        break;
-       
-    case 3:
-    cout<<"Enter the Roll number of student to updates the details: "<<endl;
-    cin>>modify;
-    flags=1;
-    while(flags==1)
-    {
 
-    for(i=0;i<n;i++)
-    {
-    r=s[i].search();
-    if(r==modify)
-    {
-    cout<<"---------MENU----------"<<endl;
-            cout<<"1:Change the name "<<endl;
-           cout<<"2:Change the Date of Birth"<<endl;
-           cout<<"3:Change the Address of the student "<<endl;
-           cout<<"4:Change the Driving license of the student "<<endl;
-           cout<<"5:Change the Phone number of the student "<<endl;
-           cout<<"6: Exit"<<endl;
-           cout<<"ENTER YOUR CHOICE :"<<endl;
-           cin>>option;
-    switch(option)
-    {
-    case 1:
-    s[i].update_name();
-    cout<<"----------------------------"<<endl;
-    s[i].display();
-    cout<<"----------------------------"<<endl;
-    break;
-   
-    case 2:
-    s[i].update_dob();
-    cout<<"----------------------------"<<endl;
-    s[i].display();
-    cout<<"----------------------------"<<endl;
-       break;
-       
-    case 3:
-    s[i].update_addr();
-    cout<<"----------------------------"<<endl;
-    s[i].display();
-    cout<<"----------------------------"<<endl;
-    break;
-   
-    case 4:
-    s[i].update_licno();
-    cout<<"----------------------------"<<endl;
-    s[i].display();
-    cout<<"----------------------------"<<endl;
-    break;
-   
-    case 5:
-    s[i].update_phn();
-    cout<<"----------------------------"<<endl;
-    s[i].display();
-    cout<<"----------------------------"<<endl;
-    break;
-   
-    case 6:
-    flags=0;
-    break;
-   
-    default:
-    cout<<"Incorrect choice Please enter the appropiate choice ";
-}
-}
-}
-    }
-    break;
-   
-    case 4:
-    cout<<"The Roll number of student whose data should be delete: ";
-    cin>>del;
-   
-    for(i=0;i<n;i++)
-    {
-    r=s[i].search();
-    if(r==del)
-    {
-       for(j=i;j<n-1;j++)
-       {
-    s[j]=s[j+1];
-    }
-    n--;
-}
-}
-for(i=0;i<n;i++)
-{
-   s[i].display();
-   cout<<"----------------------------"<<endl;
-}
-
-break;
-
-    case 5:
-     cout<<"Thank You"<<endl;
-    flag=0;
-    break;
-   
-default:
-    cout<<"Incorrect choice Please enter the appropiate choice ";
-
-    }
-}
+    } while (k != 5);
+    return 0;
 }
